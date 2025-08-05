806c0ee2ebb22b5b7735a998c0be119812e47667
Divya Rathod <drtas2521@gmail.com>
2025-08-05 19:40:00 +0530

    Feat : Added transcription api for youtube videos

---
+++ Transcriber/youtube_transcriber.py
@@ -0,0 +1,23 @@
+"""
+This file generates transcripts based on youtube video links
+"""
+import re
+from youtube_transcript_api import YouTubeTranscriptApi
+
+class YoutubeTranscriber:
+    def __init__(self):
+        pass
+
+    def get_video_id(self, url : str ) -> str:
+        match = re.search(r"v=([^&]+)",url)
+        if not match:
+            raise ValueError("Invalid Youtube URL")
+        
+        video_id = match.group(1)
+        return video_id
+
+    def get_transcript(self,video_id : str) -> str:
+        ytt_api = YouTubeTranscriptApi()
+        transcript_list = ytt_api.fetch(video_id)
+        transcript = " ".join(snippet.text for snippet in transcript_list)
+        return transcript
